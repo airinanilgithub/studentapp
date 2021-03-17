e16b139f2328e5a162d92566f07d85a1fbfcafdd
airinanilgithub <mca501@rajagiri.edu>
2021-03-17 13:06:22 +0530

    message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,25 @@
     <title>Document</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-primary bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Student</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/contact">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/faculty">Faculty</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
     <div class="container">
     <div class="table">
     <tr>
